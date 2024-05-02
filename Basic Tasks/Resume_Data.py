ec34231e0048a374ba985998fa73f97ca80f7149
Muhammad Harris Rashid <125113774+commando839@users.noreply.github.com>
2024-05-02 20:48:54 +0500

    Add files via upload

---
+++ Basic Tasks/Resume_Data.py
@@ -0,0 +1,64 @@
+data = {
+    "Name": "Muhammad Harris Rashid",
+    "Profile Headline": "FCCU' 25 | Aspiring Biotechnologist | Python Developer at YoungDev Interns | Avid & Skilled Researcher",
+    "Contact": {
+        "Mobile": "+923058111785",
+        "Email": "commando839@gmail.com",
+        "LinkedIn": "www.linkedin.com/in/muhammadharris-rashid-ab49b123a",
+        "GitHub": "https://github.com/commando839"
+    },
+    "Experience": [
+        {
+            "Company": "YoungDev Intern",
+            "Position": "Python Developer",
+            "Duration": "April 2024 - Present (2 months)",
+            "Location": "Islāmābād, Pakistan",
+            "Description": "Working on multiple projects, each project being more complex and difficult than the next one."
+        },
+        {
+            "Company": "CodSoft",
+            "Position": "Python Developer",
+            "Duration": "February 2024 - March 2024 (2 months)",
+            "Location": "West Bengal, India",
+            "Description": "Sharpened python skills and used them in developing practical applications."
+        },
+        {
+            "Company": "SOFTEC",
+            "Position": "Brand Ambassador",
+            "Duration": "January 2024 - February 2024 (2 months)",
+            "Location": "Lahore District, Punjab, Pakistan",
+            "Description": "Brand ambassador role at SOFTEC'24 at FCCU, sharpened communication and marketing skills."
+        },
+        {
+            "Company": "Freelance (Self employed)",
+            "Position": "Content Writer / Data Entry / Data Analysis / Research",
+            "Duration": "February 2019 - February 2024 (5 years 1 month)",
+            "Location": "Lahore, Punjab, Pakistan",
+            "Description": "Experienced freelance professional adept at web content creation, research, and data analysis."
+        },
+        {
+            "Company": "Pakistan Council of Scientific and Industrial Research (PCSIR)",
+            "Position": "Summer Intern",
+            "Duration": "July 2023 - September 2023 (3 months)",
+            "Location": "Lahore, Punjab, Pakistan",
+            "Description": "Internship at PCSIR's prestigious Food & Biotechnology Research Centre (FBRC)."
+        }
+    ],
+    "Education": [
+        {
+            "Institution": "Forman Christian College (A Chartered University)",
+            "Degree": "Bachelor of Science - BS, Biotechnology",
+            "Duration": "(September 2021 - June 2025)"
+        },
+        {
+            "Institution": "Government College University (GCU), Lahore",
+            "Degree": "Intermediate, Fsc. Pre Medical",
+            "Duration": "(September 2019 - August 2021)"
+        },
+        {
+            "Institution": "Ibne Sina College",
+            "Degree": "O-levels, Mathematics & Sciences",
+            "Duration": "(March 2007 - July 2019)"
+        }
+    ]
+}
